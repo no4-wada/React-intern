0ba70cc72a5b89f4a93ad746655dc70086ab5bac
kikuno <kikuno@no4.co.jp>
2024-04-23 12:17:56 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,150 +1,192 @@
-import { useCallback, useState } from "react";
+import type { EventContentArg, EventInput } from "@fullcalendar/core";
 import FullCalendar from "@fullcalendar/react";
-
-import type { DateSelectArg, EventApi, EventClickArg } from "@fullcalendar/core"
-
+import { useCallback, useState } from "react";
+import type { EventClickArg } from "@fullcalendar/core";
+import allLocales from "@fullcalendar/core/locales-all";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import allLocales from '@fullcalendar/core/locales-all';
-import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
-import { INITIAL_EVENTS, createEventId } from './event-utils'
+import type { DateClickArg } from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
+import { EventImpl } from "@fullcalendar/core/internal";
 
-// モーダルを表示する関数
-function showModal() {
-    modal!.style.display = 'block';
+function renderEventContent(eventInfo: EventContentArg) {
+  return (
+    <div>
+      <div>タイトル: {eventInfo.event._def.title}</div>
+      <div>内容: {eventInfo.event._def.extendedProps.content}</div>
+      <div>開始時刻: {eventInfo.event._def.extendedProps.startDate}</div>
+      <div>終了時刻: {eventInfo.event._def.extendedProps.endDate}</div>
+    </div>
+  );
 }
 
-// モーダルを非表示にする関数
-function hideModal() {
-    modal!.style.display = 'none';
-}
-
-// モーダルの表示
-const modal = document.getElementById('modal');
-const closeModal = document.getElementById('closeModal');
 
 function App() {
+  // モーダルの開閉状態を管理
+  const [showModal, setShowModal] = useState<boolean>(false);
 
-  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
-  const handleEvents = useCallback((events: EventApi[]) =>
-    setCurrentEvents(events)
-  , []);
+  // モーダルを管理
+  const [isShowedAddModal, setisShowAddModal] = useState<boolean>(false);
+  // イベントの管理
+  const [events, setEvents] = useState<EventInput[]>([]);
 
-  // カレンダーをクリックしたときの処理
-  let countClick:boolean = true;
-  // if( countClick == true){
-  const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
-    
+  // イベントの管理
+  const [deleteEvent, setdeleteEvents] = useState<EventImpl>();
+
+  // イベント日の管理
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  // モーダルの内容を管理
+  // [要素、更新関数] = useState< 型 >("初期値")
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+
+  const handleDateClick = useCallback(({ date }: DateClickArg): void => {
+    // イベント日をセット
+    setSelectedDate(date);
+    console.log(date)
+
     // モーダルウィンドウ(追加)の表示
-    showModal();
+    setShowModal(true);
+    setisShowAddModal(true);
+  }, [isShowedAddModal]);
 
-    // モーダルウィンドウを閉じる
-    closeModal!.addEventListener('click', hideModal);
+  // 送信する押下時処理
+  const handleClickSendButton = useCallback((): void => {
+    // 選択された日がない場合はなにもせずreturn
+    if (!title && !content) {
+      return;
+    }
 
-    const formArea = document.querySelector('form');
-    
-    // フォームが入力された時の処理
-    formArea!.addEventListener('submit', (e) => {
-      
-      // 選択したカレンダーの情報を取得
-      const calendarApi = selectInfo.view.calendar;
-      // calendarApi.unselect();
-
-      e.preventDefault();
-
-      // イベント内容の取得
-      const title : string= e.target.title.value;
-      const content :string = e.target.content.value;
-      const start :string = e.target.start.value;
-      const end :string = e.target.end.value;
-      
-      //if (title && content && start && end) {}
-        calendarApi.addEvent({
+    // イベント追加
+    setEvents((prev: EventInput[]): EventInput[] => [
+      ...prev,
+      {
         id: createEventId(),
         title,
         content,
-        // 要確認
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-        })
-      // console.log(end)
-      // console.log(start)
-      e.target.title.value = ""
+        startDate,
+        endDate,
+        date: selectedDate,
+      },
+    ]);
 
-      
-      calendarApi.unselect();
-      }
-    )
-      // console.log("2");
-  }, []);
+    // イベント内容をクリア
+    setSelectedDate(null);
+    setTitle("");
+    setContent("");
+    setStartDate("");
+    setEndDate("");
 
-  const handleDateClick = useCallback((selectInfo: DateClickArg) => {
-    // モーダルウィンドウ(追加)の表示
-    showModal();
+    // モーダルを閉じる
+    setShowModal(false);
+    setisShowAddModal(false);
+  }, [content, endDate, selectedDate, startDate, title]);
 
-    // モーダルウィンドウを閉じる
-    closeModal!.addEventListener('click', hideModal);
-
-    const formArea = document.querySelector('form');
-    
-    // フォームが入力された時の処理
-    formArea!.addEventListener('submit', (e) => {
-      
-      // 選択したカレンダーの情報を取得
-      const calendarApi = selectInfo.view.calendar;
-      // calendarApi.unselect();
-
-      e.preventDefault();
-
-      // イベント内容の取得
-      const title : string= e.target.title.value;
-      const content :string = e.target.content.value;
-      
-      //if (title && content && start && end) {}
-        calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        content,
-        // 要確認
-        start: selectInfo.date,
-        end: selectInfo.date,
-        // allDay: selectInfo.allDay,
-        })
-      e.target.title.value = ""
-        console.log(selectInfo.date);
-      
-      calendarApi.unselect();
-      }
-    )
-  }, []);
-// }
-  // イベントクリック処理
+  // イベントクリック時の表示処理
   const handleEventClick = useCallback((clickInfo: EventClickArg) => {
 
     // 編集前のイベントを取得
     const beforeTitle : string= clickInfo.event._def.title
-    const beforeContent :string = clickInfo.event._def.extendedProps.content;
-    const beforeStart :string = clickInfo.event.startStr;
-    const beforeEnd :string = clickInfo.event.endStr;  
-    // console.log (clickInfo.event._instance.range.end);
+    const beforeContent :string = clickInfo.event._def.extendedProps.content
+    const beforeStartDate :string = clickInfo.event.extendedProps.startDate
+    const beforeEndDate :string = clickInfo.event.extendedProps.endDate  
+    
+    const deleteEvent :EventImpl = clickInfo.event
+
+    // 各要素にセット
+    setTitle(beforeTitle);
+    setContent(beforeContent);
+    setStartDate(beforeStartDate);
+    setEndDate(beforeEndDate);
+
+    setdeleteEvents(deleteEvent);
+
+    // こっちでの削除はできます
+    //deleteEvent.remove()
+    
+    console.log(clickInfo.event._def.defId)
+
     // モーダルウィンドウ(編集)の表示
-    showModal();
-    closeModal!.addEventListener('click', hideModal);
+    setShowModal(true);
   }, []);
 
-  /*/ イベント削除時の処理
-  const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+  // イベント削除時の処理
+  const handleClickDeleteButton = useCallback((): void => {
     if (
-      window.confirm(`このイベント「${clickInfo.event.title}」を編集しますか`)
+      window.confirm(`このイベント「」を削除しますか`)
     ) {
-      clickInfo.event.remove();
+
+      deleteEvent?.remove();
+      }
+  },[]);
+
+
+  /*/ 更新する押下時処理
+  const handleClickEditButton = useCallback((): void => {
+    // イベントの中身がない場合はなにもせずreturn
+    if (!title && !content) {
+      return;
     }
-  }, []);
+  
+  
+  // イベント更新
+    setEvents((prev: EventInput[]): EventInput[] => [
+      ...prev,
+      {
+        id: createEventId(),
+        title,
+        content,
+        startDate,
+        endDate,
+        date: selectedDate,
+      },
+    ]);
+
+    // イベント内容をクリア
+    setSelectedDate(null);
+    setTitle("");
+    setContent("");
+    setStartDate("");
+    setEndDate("");
+
+    // モーダルを閉じる
+    setShowModal(false);
+  }, [content, endDate, selectedDate, startDate, title]);
   /*/
+
+  const handleChangeTitle = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setTitle(event.target.value);
+    },
+    []
+  );
+
+  const handleChangeContent = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+      setContent(event.target.value);
+    },
+    []
+  );
+
+  const handleChangeStartDate = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setStartDate(event.target.value);
+    },
+    []
+  );
+
+  const handleChangeEndDate = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setEndDate(event.target.value);
+    },
+    []
+  );
 
   // リターン処理
   return (
-    
     <div className="demo-app">
       <div className="demo-app-main">
         <FullCalendar
@@ -157,12 +199,137 @@ function App() {
           initialEvents={INITIAL_EVENTS}
           locales={allLocales}
           locale="ja"
-          eventsSet={handleEvents}
           dateClick={handleDateClick}
-          // select={handleDateSelect}
           eventClick={handleEventClick}
+          events={events}
+          eventContent={renderEventContent}
         />
       </div>
+
+      {/* showModal,isShowedAddModalがtrueのときだけ表示 */}
+      {showModal && isShowedAddModal && (
+        <div className="modal">
+          <form>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">タイトル</p>
+              <input
+                id="modal_title_input"
+                type="text"
+                className="Form-Item-Input"
+                data-dismiss="modalTitle"
+                name="title"
+                value={title}
+                onChange={handleChangeTitle}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">内容</p>
+              <textarea
+                className="Form-Item-Textarea"
+                cols={20}
+                rows={3}
+                name="content"
+                value={content}
+                onChange={handleChangeContent}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">開始時刻</p>
+              <input
+                type="text"
+                className="Form-Item-Input"
+                placeholder="例）HH-MM-SS"
+                value={startDate}
+                onChange={handleChangeStartDate}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label isMsg">終了時刻</p>
+              <input
+                type="text"
+                className="Form-Item-Input"
+                placeholder="例）HH-MM-SS"
+                value={endDate}
+                onChange={handleChangeEndDate}
+              />
+            </div>
+            <button
+              type="button"
+              className="Form-Btn"
+              onClick={() => handleClickSendButton()}
+            >
+              送信する
+            </button>
+          </form>
+          <button type="button" onClick={() => setShowModal(false)}>
+            閉じる
+          </button>
+        </div>
+      )}
+
+      {/* showModalがtrue,isShowedAddModalがfalseのときだけ表示 */}
+      {showModal && !isShowedAddModal && (
+        <div className="modal">
+          <form>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">タイトル</p>
+              <input
+                id="modal_title_input"
+                type="text"
+                className="Form-Item-Input"
+                data-dismiss="modalTitle"
+                name="title"
+                value={title}
+                onChange={handleChangeTitle}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">内容</p>
+              <textarea
+                className="Form-Item-Textarea"
+                cols={20}
+                rows={3}
+                name="content"
+                value={content}
+                onChange={handleChangeContent}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label">開始時刻</p>
+              <input
+                type="text"
+                className="Form-Item-Input"
+                placeholder="例）HH-MM-SS"
+                value={startDate}
+                onChange={handleChangeStartDate}
+              />
+            </div>
+            <div className="Form-Item">
+              <p className="Form-Item-Label isMsg">終了時刻</p>
+              <input
+                type="text"
+                className="Form-Item-Input"
+                placeholder="例）HH-MM-SS"
+                value={endDate}
+                onChange={handleChangeEndDate}
+              />
+            </div>
+            <button
+              type="button"
+              className="Form-Btn"
+              onClick={() => handleClickSendButton()}
+            >
+              更新する
+            </button>
+          </form>
+          <button type="button" onClick={() => setShowModal(false)}>
+            閉じる
+          </button>
+          <button type="button" id="deleteButton" onClick={() => handleClickDeleteButton()}>
+            削除する
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/event-utils.ts
+++ src/event-utils.ts
@@ -1,7 +1,7 @@
-import { EventInput } from "@fullcalendar/react";
+import type { EventInput } from "@fullcalendar/core";
 
 let eventGuid = 0;
-const todayStr = new Date().toISOString().replace(/T.*$/, "");  // 今日の日付をYYYY-MM-DD形式にする
+const todayStr = new Date().toISOString().replace(/T.*$/, ""); // 今日の日付をYYYY-MM-DD形式にする
 export const createEventId = () => String(eventGuid++);
 export const INITIAL_EVENTS: EventInput[] = [
   {
@@ -14,6 +14,8 @@ export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
     title: "Timed event",
-    start: todayStr + "T12:00:00",  // 時刻はTで結ぶ
+    content: "Timedcontent",
+    startDate: todayStr + "T4:00:00", // 時刻はTで結ぶ
+    endDate: todayStr + "T11:00:00",
   },
 ];
